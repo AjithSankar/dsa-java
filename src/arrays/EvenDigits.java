b088e0d7ff8c9fcc5148d4961d99dbf622144e76
AjithSankar <mailajithkumars@gmail.com>
2024-12-04 17:31:22 +0530

    LeetCode-1295- Arrays find even digits

---
+++ src/arrays/EvenDigits.java
@@ -0,0 +1,48 @@
+package arrays;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits
+
+public class EvenDigits {
+
+    public static void main(String[] args) {
+        int[] nums = {5557, 901, 482, 1771};
+        //System.out.println(findNumbers(nums));
+        System.out.println(findEvenNumbers(nums));
+    }
+
+    static int findEvenNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean even(int num) {
+        int numberOfDigits = digits(num);
+        return numberOfDigits % 2 == 0;
+    }
+
+    private static int digits(int num) {
+        int digits = 0;
+        while (num > 0) {
+            digits++;
+            num = num / 10;
+        }
+        return digits;
+    }
+
+    // Simple Approach
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            String str = Integer.toString(nums[i]);
+            if (str.length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
