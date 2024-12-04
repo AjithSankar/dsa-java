70962b05dcaaf1c5871b5795e5b5fbe20d4d3813
AjithSankar <mailajithkumars@gmail.com>
2024-12-04 18:18:29 +0530

    LeetCode-1672- Arrays - Richest Customer Wealth

--- src/arrays/MaxWealth.java
+++ src/arrays/MaxWealth.java
@@ -13,13 +13,22 @@ public class MaxWealth {
     }
 
     private static int maxWealth(int[][] accounts) {
+
+        // person = row
+        // accounts(bank) = col
+
         int maxSum = 0;
         for (int[] person : accounts) {
+            // Take new sum for the row
             int currentSum = 0;
 
             for (int account : person) {
                 currentSum += account;
             }
+
+            // now we have sum of accounts of person
+            // compare with maxSum
+
             if (currentSum > maxSum) {
                 maxSum = currentSum;
             }
