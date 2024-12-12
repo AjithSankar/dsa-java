1e135d52510387e9e6a939dd050d607e82c87f1a
Hemalatha Mani <hemalatha.mani@banfico.com>
2024-12-12 22:35:04 +0530

    Leetcode Rotated sorted array

---
+++ src/binarySearch/RotatedSortedArray.java
@@ -0,0 +1,64 @@
+package binarySearch;
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+public class RotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(arr, 0));
+
+    }
+
+    public static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        int firstTry = binarySearch(nums, target, 0, pivot);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    public static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // case 1
+            if (mid < end && arr[mid] > arr[mid + 1]) { // To avoid ArrayOutOfBoundsException
+                return mid;
+            }
+
+            // case 2
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // case 3
+            if (arr[mid] <= arr[start]) {
+                end = mid - 1;
+            } else { // case 4
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
