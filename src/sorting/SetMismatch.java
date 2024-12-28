8c5df186134dc909d248310b1b997ef2ff486db6
AjithSankar <mailajithkumars@gmail.com>
2024-12-28 17:32:55 +0530

    Leetcode- 645. Set Mismatch

---
+++ src/sorting/SetMismatch.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/set-mismatch/
+public class SetMismatch {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 3, 3, 3};
+        System.out.println(Arrays.toString(findErrorNums(arr)));
+
+    }
+
+    public static int[] findErrorNums(int[] nums) {
+
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return new int[] {nums[index], index+1};
+            }
+        }
+
+        return new int[]{};
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
