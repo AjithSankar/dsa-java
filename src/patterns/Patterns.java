49b40f912ba2762c93cea3f5365030c2e309b345
AjithSankar <mailajithkumars@gmail.com>
2024-12-31 16:47:24 +0530

    Diamond Pattern 28

--- src/patterns/Patterns.java
+++ src/patterns/Patterns.java
@@ -24,11 +24,29 @@ public class Patterns {
         pattern4(5);
         System.out.println();
         pattern5(5);
+        System.out.println();
+        pattern28(5);
+    }
+
+    static void pattern28(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            for (int col = 1; col <= totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 
     static void pattern5(int n) {
         for (int row = 1; row <= 2 * n; row++) {
-            int totalColsInRow = row > n ? 2 * n - row: row;
+            int totalColsInRow = row > n ? 2 * n - row : row;
             for (int col = 1; col <= totalColsInRow; col++) {
                 System.out.print("* ");
             }
